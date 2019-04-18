6261f382ceead8acfe562f94ac96e7e6d9353382
Patrick McCauley <pmccau@upenn.edu>
2019-04-18 17:52:18 -0400

    Added in some comments

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -10,6 +10,13 @@ public class UserInterface implements Runnable {
 
 	private JFrame frame = new JFrame("PhillyOpenData Analyzer");
 	
+	/**
+	 * This will be the top-level container for the GUI. 
+	 * This panel will have multiple sub-panels and buttons embedded
+	 * within it, but will serve as the main housing for all those
+	 * that will drive functionality
+	 * @return
+	 */
 	public JPanel topLevel() {
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(3, 1));
@@ -21,19 +28,21 @@ public class UserInterface implements Runnable {
 		JPanel slicerPanel = new JPanel();
 		slicerPanel.setLayout(new GridLayout());
 		
+		// Could store the buttons/event listeners in a HashMap
 		HashMap<String, EventListener> slicers = new HashMap<>();
 		slicers.put("test", new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("hit the test");
 			}
 		});
-		
+	
 		slicers.put("second one", new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Hit the second one");
 			}
 		});
 		
+		// Adding in the actual stuff from the HashMap
 		for (String str : slicers.keySet()) {
 			Checkbox temp = new Checkbox(str);
 			slicerPanel.add(temp);
@@ -53,19 +62,23 @@ public class UserInterface implements Runnable {
 		return panel;
 	};
 	
-	// Run method
+	// Run method. This will be called to generate the GUI
 	public void run() {
 		frame.add(topLevel());
 		
-		frame.setPreferredSize(new Dimension(745, 375));
-		frame.setMaximumSize(frame.getPreferredSize());
-		frame.setResizable(false);
+//		frame.setPreferredSize(new Dimension(745, 375));
+//		frame.setMaximumSize(frame.getPreferredSize());
+//		frame.setResizable(false);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
 	}
 	
+	/**
+	 * Main method. Funtion TBD 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new UserInterface());
 	}
