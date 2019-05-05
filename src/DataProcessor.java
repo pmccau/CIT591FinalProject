cf63d2e5fc20466f24f706b19fb5f1b9a2aba811
esbickel <46583340+esbickel@users.noreply.github.com>
2019-05-05 01:37:15 -0400

    Update DataProcessor.java

--- src/DataProcessor.java
+++ src/DataProcessor.java
@@ -3,27 +3,56 @@ import java.util.*;
 public class DataProcessor {
 
 	private HashMap<String, ArrayList<String>> fullDataSet;
-	private HashMap<String, ArrayList<String>> relevantData;
+	private HashMap<String, ArrayList<String>> relevantData; // Each school ID, points to list of data
+	private ArrayList<String> SearchParameters;
 	
 	public DataProcessor (HashMap<String, ArrayList<String>> dataSetIn) {
+		// CONFIRM WITH YU - Input will be HashMap with School IDs as key and full list as ArrayList???
 		fullDataSet = dataSetIn;
+		SearchParameters = new ArrayList<String>() ;
 	}
 	
 	
 	
-	// SEARCHING METHODS
+	// INTERACTIONS: Input (searching) and Output (graphing, numerical analysis)
 	
 	/**
 	 * Sets the values of 'relevantData' based on one search criteria
 	 * 
 	 * @param column
-	 * @param exactValue
-	 */
-	private void SearchParameter(String column, String comparison, String Value, String AndOr) {
-		//Add values to fullDataSet if they match the search criteria
-	
-	
+	 * @param comparison 
+	 * @param Value 
+	 * @param AndOr 
+	 */	
+	private void Search(String column, String comparison, String Value, String AndOr) {
+		SearchParameters.add(column);
+		
+		// Add schools to relevantData if they match the search criteria **************
+		// Eliminate from relevantData if AND, adding from fullDataSet if OR
+		
 	}
+	
+	/**
+	 * Uses search parameters to decide which graphs to create
+	 * Creates those graphs, and returns them (ID of map type, and graph)
+	 * 
+	 */
+	private HashMap<String, Graph> GetGraphs() {	
+	
+		// Use search parameters to decide **************
+	}
+	
+	/**
+	 * Uses search parameters to decide which summary analysis to complete
+	 * Completes and returns that analysis (ID of analysis, then value)
+	 * 
+	 */
+	private HashMap<String, String> GetSummary() {	
+	
+		// Use search parameters to decide **************
+	}
+	
+	
 	
 	
 	// BASIC ANALYSIS METHODS
@@ -69,7 +98,25 @@ public class DataProcessor {
 	 * @return
 	 */
 	public Graph PieChartInfo (String column) {
-		
+		int columnInt = Integer.parseInt(column);
+		HashMap<String, Double> data = new HashMap<String, Double>();
+		double count = 0;
+		for (String School : relevantData.keySet()) {
+			for (String category : data.keySet()) {
+				if (relevantData.get(School).get(columnInt).equals(category)) {
+					data.replace(category, data.get(category)+1);
+				}
+				else {
+					data.put(category, 1.0);
+				}
+				count = count +1;
+			}
+		}
+		for (String category : data.keySet()) {
+			data.replace(category, data.get(category)/count);
+		}
+		Graph pieChart = new Graph ("Pie Chart", data);
+		return pieChart;
 	}
 	
 	/**
@@ -77,7 +124,30 @@ public class DataProcessor {
 	 * @return
 	 */
 	public Graph BarChartInfo (String X_column, String Y_column) {
+		int XColumnInt = Integer.parseInt(X_column);
+		int YColumnInt = Integer.parseInt(Y_column);
 		
+		// If not a value, enter as empty string: ""
+		
+		// Create graph ***********
+		Graph barChart = new Graph ("Bar Chart", data);
+		return barChart;
+	}
+	
+	/**
+	 * Returns a hashmap of each unique value in a column, and the corresponding percent
+	 * @return
+	 */
+	public Graph ScatterPlotInfo (String X_column, String Y_column) {
+		int XColumnInt = Integer.parseInt(X_column);
+		int YColumnInt = Integer.parseInt(Y_column);
+		
+		// If not a value, enter as empty string: ""
+
+		// Create graph *************** 
+		
+		Graph scatterPlot = new Graph ("Scatter Plot", data);
+		return scatterPlot;
 	}
 	
 }
